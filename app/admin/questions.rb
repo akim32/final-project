5ebb99f1135fc66ef6f4feb5f68789a47c7bf914
Murugan <murugan@firstdraft.com>
2018-03-14 21:21:47 +0000

    redis config

--- app/admin/questions.rb
+++ app/admin/questions.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Question do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :state, :question, :answer, :answer_explanation
+permit_params :state, :question, :answer, :answer_explanation, :chat_room_id
 #
 # or
 #
